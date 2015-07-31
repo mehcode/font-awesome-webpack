ac7fc44b9eddff007ca122a8be7cf0c171b7316d
Ryan Leckey <leckey.ryan@gmail.com>
2015-07-31 00:37:49 -0700

    Remove less config hook

--- font-awesome-styles.loader.js
+++ font-awesome-styles.loader.js
@@ -18,8 +18,7 @@ module.exports = function(content) {
     var config = this.exec(content, this.resourcePath);
     var start =
             "@import          \"~font-awesome/less/variables.less\";\n"
-            + "@fa-font-path: \"~font-awesome/fonts/\";\n"
-            + "@import          \"./font-awesome.config.less\";\n";
+            + "@fa-font-path: \"~font-awesome/fonts/\";\n";
     source = start + styles.filter(function(style) {
         return config.styles[style];
     }).map(function(style) {
